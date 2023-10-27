f1753f3bed7ca0ac2fe6415d08625d865aed858e
Yared Kebede <135129440+Yareda21@users.noreply.github.com>
2023-10-27 21:08:52 +0300

    update

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -40,42 +40,44 @@ const OpenCards = () => {
   ];
 
   return (
-    <section className="absolute top-10  w-full z-40 py-1 pb-[300px] ">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-        <h1 className="text-3xl font-extrabold text-white">Services</h1>
-      </div>
-      <div className="block m-auto justify-center items-center overflow-scroll z-30 h-[550px] md:overflow-auto">
-        <div className="mt-2  flex flex-col md:flex-row justify-center items-center gap-5">
-          {[0, 1, 2, 3, 4].map((index) => (
-            <motion.div
-              key={index}
-              className={`card cursor-pointer h-[500px] bg-cover bg-center rounded-[20px] ${
-                index === expandedIndex ? "expanded" : ""
-              }`}
-              variants={cardVariants}
-              initial="collapsed"
-              animate={index === expandedIndex ? "expanded" : "collapsed"}
-              transition={{ duration: 0.5 }}
-              onMouseEnter={() => handleCardHover(index)}
-              onMouseLeave={handleCardLeave}
-              style={{
-                backgroundImage: `url(${cardImages[index]})`,
-              }}
-            >
-              <div className="card-content h-full flex flex-col justify-end">
-                <div className="card-footer rounded-b-[20px] bg-gray-800 bg-opacity-75 min-h-[100px] flex flex-col items-center justify-center">
-                  <h2 className="text-xl font-semibold text-white text-center">
-                    {service[index]}
-                  </h2>
-                  {index === expandedIndex && (
-                    <p className="mt-2 text-gray-300 text-justify px-10">
-                      {cardDescriptions[index]}{" "}
-                    </p>
-                  )}
+    <section className="flex flex-col justify-center items-center h-full w-full px-5 ">
+      <div className="z-40 sm:mt-12">
+        <div className="max-w-7xl mx-auto px-4 my-1 sm:px-6 lg:px-8 text-center">
+          <h1 className="text-3xl font-extrabold text-white">Services</h1>
+        </div>
+        <div className="block m-auto h-[400px] w-full overflow-auto px-5">
+          <div className="flex flex-col md:flex-row md:h-[400px] justify-center items-center gap-5">
+            {[0, 1, 2, 3, 4].map((index) => (
+              <motion.div
+                key={index}
+                className={`card cursor-pointer h-[350px] bg-cover bg-center rounded-[20px] ${
+                  index === expandedIndex ? "expanded" : ""
+                }`}
+                variants={cardVariants}
+                initial="collapsed"
+                animate={index === expandedIndex ? "expanded" : "collapsed"}
+                transition={{ duration: 0.5 }}
+                onMouseEnter={() => handleCardHover(index)}
+                onMouseLeave={handleCardLeave}
+                style={{
+                  backgroundImage: `url(${cardImages[index]})`,
+                }}
+              >
+                <div className="card-content h-full flex flex-col justify-end">
+                  <div className="card-footer rounded-b-[20px] bg-gray-800 bg-opacity-75 min-h-[100px] flex flex-col items-center justify-center">
+                    <h2 className="text-xl font-semibold text-white text-center">
+                      {service[index]}
+                    </h2>
+                    {index === expandedIndex && (
+                      <p className="mt-2 text-gray-300 text-justify px-10">
+                        {cardDescriptions[index]}{" "}
+                      </p>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </motion.div>
-          ))}
+              </motion.div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
