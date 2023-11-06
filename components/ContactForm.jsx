b67615edc1c202bb9e4c5995866133bcfb7b2ba0
Yared Kebede <135129440+Yareda21@users.noreply.github.com>
2023-11-06 18:04:38 +0300

    push

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 
+
 const ContactForm = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
@@ -27,7 +28,7 @@ const ContactForm = () => {
 
   return (
     <div className="w-[100%] md:w-[80%] mx-auto">
-      <form className="py-4 mx-auto w-[90%] md:w-[50%] md:mx-0">
+      <form className="flex flex-col gap-2 py-4 z-30 mx-auto w-[90%] md:w-[50%] md:mx-0">
         <div>
           <label htmlFor="fullname">Full Name</label>
           <input
@@ -38,6 +39,7 @@ const ContactForm = () => {
             placeholder="Yared Kebede"
           />
         </div>
+
         <div>
           <label htmlFor="phone">Phone Number:</label>
           <input
@@ -59,6 +61,7 @@ const ContactForm = () => {
             placeholder="ykebed40@gmail.com"
           />
         </div>
+
         <div>
           <label htmlFor="message">Message</label>
           <textarea
@@ -70,6 +73,7 @@ const ContactForm = () => {
             placeholder="Type your message here...."
           ></textarea>
         </div>
+        
         <button
           className="bg-green-700 mt-1 mx-auto px-4 text-white font-bold"
           type="submit"
@@ -83,6 +87,15 @@ const ContactForm = () => {
         <div className="text-red-600 px-5 py-2 w-[90%] md:w-[50%] md:mx-0">
           Error Message
         </div>
+      </div>
+
+      <div className="hidden absolute top-[150px] md:flex right-[100px] w-[350px] h-[450px]">
+        <video autoPlay loop
+          type="video/mp4"
+          src="/video.mp4"
+          width="100%"
+          height="100%"
+        />
       </div>
     </div>
   );
