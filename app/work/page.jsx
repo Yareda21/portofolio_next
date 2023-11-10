f04296a281bfeeee200fd614b7b93e9ec2ff4be0
Yared Kebede <135129440+Yareda21@users.noreply.github.com>
2023-11-10 09:22:27 +0300

    update

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,63 +1,81 @@
 "use client";
+import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import Modal from "@/components/Modal";
 
-const subjects = [
-  {
-    name: "HTML, CSS and JvaScript Projects",
-    details: "The project details is here",
-  },
-  {
-    name: "React Projects",
-    details: "The project details is here",
-  },
-  {
-    name: "Python Projects and Codes",
-    details: "The project details is here",
-  },
+const department = [
+  { name: "HTML, CSS and JavaScript", description: ["des of html and css"] },
+  { name: "React.js" },
+  { name: "Next.js" },
+  { name: "Python Code" },
 ];
 
-const App = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const close = () => setModalOpen(false);
-  const open = () => setModalOpen(true);
-
+const page = () => {
   
-  const [index, setIndex] = useState(0)
   return (
-    <div className="absolute top-0 left-0 w-full h-full">
-      <motion.div
-        // className="absolute z-10 m-auto top-[50%] left-[50%] bg-white cursor-pointer text-blue-900 px-10 py-5"
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        onClick={() => (modalOpen ? close() : open())}
-      >
-        {subjects.map((item, itemIndex) => {
-          return (
-            <div className=" ">
-              <h1
-                onClick={() => (modalOpen ? close() : open())}
-                key={itemIndex}
-              >
-                {item.name}
-              </h1>
+    <div className="grid grid-cols-3 grid-rows-1 z-30 gap-4 pt-[100px]">
+      {/* Html and cs */}
+      <div className="">
+        <h1 className="text-2xl text-accent text text-center">
+          HTML, CSS and JavaScript
+        </h1>
+        <div className="my-5 mx-auto">
+          <Link href="" target="_blank">
+            <h1 className="text-center text-lg mb-2 mt-3">Education Website</h1>
+            <div className="flex justify-center items-center w-[380px]">
+              <Image
+                src="/educa.PNG"
+                width={300}
+                height={300}
+                alt="html and css"
+                className="rounded-full mx-auto"
+              />
             </div>
-          );
-        })}
-      </motion.div>
-
-      <AnimatePresence className=" " initial={false}>
-        {modalOpen && (
-          <Modal
-            className="absolute z-40 top-[50%] left-[50%] m-auto bg-black/70"
-            modalOpen={modalOpen}
-            handleClose={close}
-          />
-        )}
-      </AnimatePresence>
+          </Link>
+        </div>
+      </div>
+      {/* react js */}
+      <div className="">
+        <h1 className="text-2xl text-accent text text-center">
+          HTML, CSS and JavaScript
+        </h1>
+        <div className="my-5">
+          <Link href="" target="_blank">
+            <h1 className="text-center text-lg mb-2 mt-3">Education Website</h1>
+            <div className="flex justify-center items-center w-[300px]">
+              <Image
+                src="/educa.PNG"
+                width={300}
+                height={300}
+                alt="html and css"
+                className="rounded-full"
+              />
+            </div>
+          </Link>
+        </div>
+      </div>
+      {/* next */}
+      <div className="">
+        <h1 className="text-2xl text-accent text text-center">
+          HTML, CSS and JavaScript
+        </h1>
+        <div className="my-5">
+          <Link href="" target="_blank">
+            <h1 className="text-center text-lg mb-2 mt-3">Education Website</h1>
+            <div className="flex justify-center items-center w-[300px]">
+              <Image
+                src="/educa.PNG"
+                width={300}
+                height={300}
+                alt="html and css"
+                className="rounded-full"
+              />
+            </div>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default App;
+export default page;
--- components/TopLeftImg.jsx
+++ components/TopLeftImg.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const TopLeftImg = () => {
   return (
-    <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[480px] opacity-50">
-      <Image src="/top-left-img.png" width={400} height={400} alt="Yared Kebede" loading='lazy' layout='responsive' objectFit='cover' objectPosition='center'/>
+    <div className="absolute left-0 top-0 mix-blend-color-dodge w-[200px] xl:w-[480px] opacity-50">
+      <Image src="/top-left-img.png" width={400} height={400} alt="Yared Kebede" loading='lazy' layout='responsive'/>
     </div>
   );
 }
