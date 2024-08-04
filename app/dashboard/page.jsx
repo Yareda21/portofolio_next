708bae162d03e171a97e25f4780955e9996c7f3d
Yared Kebede <135129440+Yareda21@users.noreply.github.com>
2024-08-04 16:55:24 +0300

    he

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -16,6 +16,7 @@ const Dashboard = ({ user }) => {
     const [course, setCourse] = useState("");
     const [testimonial, setTestimonial] = useState("");
     const [rating, setRating] = useState("");
+    const [phone, setPhone] = useState("");
     const [currentUser, setCurrentUser] = useState(user);
 
     const router = useRouter();
@@ -42,7 +43,7 @@ const Dashboard = ({ user }) => {
                 displayName: currentUser.displayName,
                 photoURL: currentUser.photoURL || "Not Available",
                 email: currentUser.email,
-                phoneNumber: currentUser.phoneNumber || "Not Available",
+                phoneNumber,
                 whereMet,
                 course,
                 testimonial,
@@ -55,6 +56,7 @@ const Dashboard = ({ user }) => {
             setCourse("");
             setTestimonial("");
             setRating("");
+            setPhone("");
             alert("Testimony Collected");
             router.push("/");
             return true;
@@ -74,25 +76,16 @@ const Dashboard = ({ user }) => {
     };
 
     return (
-        <div className="p-3 md:mt-10 mx-auto h-screen font-sans">
-            <div className="mt-[115px]">
-                <div className="md:flex md:w-[90%] justify-between p-2 rounded-lg shadow-md">
+        <div className="p-3 h-screen font-sans">
+            <div className="mt-[110px]">
+                <div className="md:flex w-[90%] justify-between p-2 rounded-lg shadow-md">
                     <h1 className="text-md md:text-3xl font-bold mb-4">
                         Wellcome to the dashboard <br />
-                        {user
-                            ? user.displayName
-                                  .split(" ")
-                                  .map(
-                                      (word) =>
-                                          word.charAt(0).toUpperCase() +
-                                          word.slice(1)
-                                  )
-                                  .join(" ")
-                            : ""}
+                        {user ? user.displayName : ""}
                     </h1>
                     <button
                         onClick={handleLogout}
-                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                        className="bg-red-500 hover:bg-red-700 text-sm text-white font-bold md:py-2 py-1 px-2  md:px-4 rounded"
                     >
                         Logout
                     </button>
@@ -100,78 +93,96 @@ const Dashboard = ({ user }) => {
 
                 <form
                     onSubmit={handleSubmit}
-                    className="flex flex-col mx-3 gap-4 md:w-[50%] md:ml-10"
+                    className="flex md:flex-row mt-4 w-full md:justify-between text-sm md:text-base h-[290px] overflow-auto flex-col mx-3 md:gap-4 gap-1 md:w-[80%] md:ml-10"
                 >
-                    <label className="flex flex-col gap-1">
-                        Where did you meet Yared?
-                        <select
-                            value={whereMet}
-                            onChange={(e) => setWhereMet(e.target.value)}
-                            className="p-2 border text-black border-gray-300 rounded-md"
-                        >
-                            <option value="">Select...</option>
-                            <option value="class">Class</option>
-                            <option value="work">Work</option>
-                            <option value="college">College</option>
-                            <option value="teacher">Teacher</option>
-                            <option value="others">Others</option>
-                        </select>
-                    </label>
-                    <label className="flex flex-col gap-1">
-                        Course attended:
-                        <select
-                            value={course}
-                            onChange={(e) => setCourse(e.target.value)}
-                            className="p-2 text-black border border-gray-300 rounded-md"
-                        >
-                            <option value="">Select...</option>
-                            <option value="python">Python</option>
-                            <option value="webDev">Web Development</option>
-                            <option value="programmingKids">
-                                Programming for Kids
-                            </option>
-                            <option value="digitalMarketing">
-                                Digital Marketing
-                            </option>
-                            <option value="dataAnalysis">
-                                Data Analysis and Visualization
-                            </option>
-                            <option value="dataScience">Data Science</option>
-                            <option value="dataScience">My Co-Worker</option>
+                    <div>
+                        <label className="flex flex-col md:my-3 gap-1">
+                            Where did you meet Yared?
+                            <select
+                                value={whereMet}
+                                onChange={(e) => setWhereMet(e.target.value)}
+                                className="p-2 border w-[400px]  text-black border-gray-300 rounded-md"
+                            >
+                                <option value="">Select...</option>
+                                <option value="class">Class</option>
+                                <option value="work">Work</option>
+                                <option value="college">College</option>
+                                <option value="teacher">Teacher</option>
+                                <option value="others">Others</option>
+                            </select>
+                        </label>
+                        <label className="flex flex-col md:my-3 gap-1">
+                            Course attended:
+                            <select
+                                value={course}
+                                onChange={(e) => setCourse(e.target.value)}
+                                className="p-2 w-[400px] text-black border border-gray-300 rounded-md"
+                            >
+                                <option value="">Select...</option>
+                                <option value="python">Python</option>
+                                <option value="webDev">Web Development</option>
+                                <option value="programmingKids">
+                                    Programming for Kids
+                                </option>
+                                <option value="digitalMarketing">
+                                    Digital Marketing
+                                </option>
+                                <option value="dataAnalysis">
+                                    Data Analysis and Visualization
+                                </option>
+                                <option value="dataScience">
+                                    Data Science
+                                </option>
+                                <option value="dataScience">
+                                    My Co-Worker
+                                </option>
 
-                            <option value="others">Others</option>
-                        </select>
-                    </label>
-                    <label className="flex flex-col gap-1">
-                        Testimonial:
-                        <textarea
-                            value={testimonial}
-                            onChange={(e) => setTestimonial(e.target.value)}
-                            placeholder="Enter your testimonial"
-                            required
-                            className="p-2 border border-gray-300 rounded-md"
-                        />
-                    </label>
-                    <label className="flex flex-col gap-1">
-                        Overall Performance
-                        <select
-                            value={rating}
-                            onChange={(e) => setRating(e.target.value)}
-                            className="p-2 border text-black border-gray-300 rounded-md"
+                                <option value="others">Others</option>
+                            </select>
+                        </label>
+                        <label className="flex flex-col md:my-3 gap-1">
+                            Testimonial:
+                            <textarea
+                                value={testimonial}
+                                onChange={(e) => setTestimonial(e.target.value)}
+                                placeholder="Enter your testimonial"
+                                required
+                                className="p-2 w-[400px] border border-gray-300 rounded-md"
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label className="flex flex-col md:my-3 gap-1">
+                            Overall Performance
+                            <select
+                                value={rating}
+                                onChange={(e) => setRating(e.target.value)}
+                                className="p-2 w-[400px] border text-black border-gray-300 rounded-md"
+                            >
+                                <option value="1">1 - Poor</option>
+                                <option value="2">2 - Fair</option>
+                                <option value="3">3 - Good</option>
+                                <option value="4">4 - Very Good</option>
+                                <option value="5">5 - Excellent</option>
+                            </select>
+                        </label>
+                        <label className="flex flex-col md:my-3 gap-1">
+                            Phone Number
+                            <input
+                                value={phone}
+                                onChange={(e) => setPhone(e.target.value)}
+                                type="tel"
+                                placeholder="Enter Phone Number...."
+                                className="p-2 w-[400px] border text-black border-gray-300 rounded-md"
+                            />
+                        </label>
+                        <button
+                            type="submit"
+                            className="p-2 mt-4 w-[400px] bg-blue-500 text-white font-bold rounded-md hover:bg-blue-700 transition-colors"
                         >
-                            <option value="1">1 - Poor</option>
-                            <option value="2">2 - Fair</option>
-                            <option value="3">3 - Good</option>
-                            <option value="4">4 - Very Good</option>
-                            <option value="5">5 - Excellent</option>
-                        </select>
-                    </label>
-                    <button
-                        type="submit"
-                        className="p-2 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-700 transition-colors"
-                    >
-                        Submit
-                    </button>
+                            Submit
+                        </button>
+                    </div>
                 </form>
             </div>
         </div>
