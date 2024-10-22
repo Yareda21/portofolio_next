201d779e3f9953f2102b20b79ccc7afa6414f0aa
Yareda21 <135129440+Yareda21@users.noreply.github.com>
2024-10-22 13:56:12 +0300

    update

--- assets/imports.js
+++ assets/imports.js
@@ -1,72 +1,90 @@
 import {
-  RxCrop,
-  RxDesktop,
-  RxPencil2,
-  RxReader,
-  RxRocket,
+    RxCrop,
+    RxDesktop,
+    RxPencil2,
+    RxReader,
+    RxRocket,
 } from "react-icons/rx";
 
 export const serviceData = [
-  {
-    icon: <RxCrop />,
-    title: "Exceed Advanced Training",
-    description: "A short term school for Computer training",
-    photo: "photo",
-    link: "https://www.exceedtraining.com.et/",
-  },
-  {
-    icon: <RxRocket />,
-    title: "Barns Eden NGO",
-    description:
-      "Website of Barns Eden NGO that helps Children affected by autism in Ethiopia ",
-    photo: "photo",
-    link: "https://barnseden.org.et/",
-  },
-  {
-    icon: <RxCrop />,
-    title: "Restaurant food Delivery",
-    description: "Help customers shop foods from any restuarant from Addis.",
-    photo: "photo",
-    link: "https://yars21.github.io/restaurant/",
-  },
-  {
-    icon: <RxPencil2 />,
-    title: "Tour and Travel Website",
-    description:
-      "This is a website for tour companies to facilitate and connect with their clients. the website provides booking, flight plan and many more.. ",
-    photo: "photo",
-    link: "https://yars21.github.io/travel/",
-  },
-  {
-    icon: <RxDesktop />,
-    title: "MIT College",
-    description:
-      "Millennials Institute of Technology website is an educational website for colleges or a university, it shows courses given by the institute and many more... ",
-    photo: "photo",
-    link: "https://yars21.github.io/school/",
-  },
-  {
-    icon: <RxReader />,
-    title: "Crud App With Next.js",
-    description:
-      "This simple app demonstrates the basics of CRUD application with MongoDB and NEXT js 14.",
-    photo: "photo",
-    link: "https://topic-crud.netlify.app/",
-  },
-  {
-    icon: <RxRocket />,
-    title: "N-Hardware",
-    description:
-      "Data collection website built for a company called N-Hardware. The website is used to collect customer feedback on companies work structure! it has both customer and admin dashboard",
-    photo: "photo",
-    link: "https://n-hardware.netlify.app/",
-  },
-  {
-    icon: <RxRocket />,
-    title: "GitHub",
-    description:
-      "This is one of my Github page, all of the source code for my projects can be found here!",
-    photo: "photo",
-    link: "https://github.com/Yareda21",
-  },
+    {
+        icon: <RxReader />,
+        title: "PFM - Finance Manager",
+        description:
+            "This Fullstack web app that demonstrates the basics of Financial Oprations and Income and expense charts.",
+        photo: "/pfm.png",
+        link: "https://pfm-yars.vercel.app/",
+    },
+    {
+        icon: <RxReader />,
+        title: "Infinity School",
+        description:
+            "A dynamic course platform built with Next.js for students to learn coding, digital marketing, and data analysis, featuring Firebase for authentication and data management.",
+        photo: "/infinity.png",
+        link: "https://yared-coding.vercel.app/",
+    },
+    {
+        icon: <RxReader />,
+        title: "Real Estate MERN App",
+        description:
+            "A full-stack real estate app built using the MERN stack with client and server-side folders, providing property listings, user authentication, and database integration.",
+        photo: "/realestate.png",
+        link: "https://realestate-yars.vercel.app/",
+    },
+    {
+        icon: <RxReader />,
+        title: "Personal Finance Manager - ALX Project",
+        description:
+            "A finance management tool using Firebase for data storage and Google authentication, allowing users to track their finances and set savings goals.",
+        photo: "/alx.png",
+        link: "https://pfm-yars.vercel.app/",
+    },
+    {
+        icon: <RxCrop />,
+        title: "Exceed Advanced Training",
+        description: "A short term school for Computer training",
+        photo: "/photo",
+        link: "https://www.exceedtraining.com.et/",
+    },
+    {
+        icon: <RxRocket />,
+        title: "Barns Eden NGO",
+        description:
+            "Website of Barns Eden NGO that helps Children affected by autism in Ethiopia ",
+        photo: "/barns.png",
+        link: "https://barnseden.org.et/",
+    },
+    {
+        icon: <RxCrop />,
+        title: "Restaurant food Delivery",
+        description:
+            "Help customers shop foods from any restuarant from Addis.",
+        photo: "/restaue.png",
+        link: "https://yars21.github.io/restaurant/",
+    },
+    {
+        icon: <RxPencil2 />,
+        title: "Tour and Travel Website",
+        description:
+            "This is a website for tour companies to facilitate and connect with their clients. the website provides booking, flight plan and many more.. ",
+        photo: "/travel.png",
+        link: "https://yars21.github.io/travel/",
+    },
+    {
+        icon: <RxDesktop />,
+        title: "MIT College",
+        description:
+            "Millennials Institute of Technology website is an educational website for colleges or a university, it shows courses given by the institute and many more... ",
+        photo: "/school.png",
+        link: "https://yars21.github.io/school/",
+    },
+
+    {
+        icon: <RxRocket />,
+        title: "GitHub",
+        description:
+            "This is one of my Github page, all of the source code for my projects can be found here!",
+        photo: "/git.png",
+        link: "https://github.com/Yareda21",
+    },
 ];
